4d1cbe3e63e12629cddcc0e7046372d204cd09ed
Divin BADIDI <divinbadidigithub@gmail.com>
2026-07-24 22:34:16 +0200

    Update create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -6,10 +6,29 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restofast_project.settings')
 django.setup()
 
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
 
 User = get_user_model()
 
-# Récupération des identifiants depuis les variables d'environnement
+# =====================================================
+# 1. NETTOYAGE ET RECRÉATION DES GROUPES
+# =====================================================
+
+print("Suppression de tous les anciens groupes...")
+Group.objects.all().delete()
+
+print("Création des nouveaux groupes officiels...")
+GROUPES_OFFICIELS = ['Serveur', 'Caissier']
+
+for nom_groupe in GROUPES_OFFICIELS:
+    Group.objects.create(name=nom_groupe)
+    print(f" -> Groupe '{nom_groupe}' créé avec succès !")
+
+
+# =====================================================
+# 2. CRÉATION DU SUPERUTILISATEUR (ADMIN)
+# =====================================================
+
 username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
 email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
 password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
