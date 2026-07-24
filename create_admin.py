13769d6f67b241ce45b5e32349ea57232789db9a
Divin BADIDI <divinbadidigithub@gmail.com>
2026-07-24 21:52:03 +0200

    Update create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -12,7 +12,7 @@ User = get_user_model()
 # Récupération des identifiants depuis les variables d'environnement
 username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
 email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'Admin1234!')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'admin')
 
 if not User.objects.filter(username=username).exists():
     print(f"Création du superutilisateur : {username}")
